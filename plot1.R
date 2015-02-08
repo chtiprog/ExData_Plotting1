eb9de007a4d7cfddc0f2df23db9f78ae6b0f75ac
spellart <chtiprog@free.fr>
2015-02-08 18:59:39 -0200

    Complete the plot1 R script and add the png file of plot1

--- plot1.R
+++ plot1.R
@@ -1,1 +1,9 @@
 data <- read.table("household_power_consumption.txt", sep = ";", col.names = colnames(read.table("household_power_consumption.txt", sep = ";", nrows = 1, header = TRUE)), skip = 1)
+data <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
+data$DataTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+data$Global_active_power = as.character(data$Global_active_power)
+data$Global_active_power = as.numeric(data$Global_active_power)
+
+png("plot1.png")
+plot1 <- hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
