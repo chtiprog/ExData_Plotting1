2231c4fd3abcb90059cdc5094f73c997306b8c77
spellart <chtiprog@free.fr>
2015-02-08 22:38:49 -0200

    Add commentaries and bg transparent

--- plot2.R
+++ plot2.R
@@ -1,6 +1,11 @@
+# Read the data after unzipped the txt file
 data <- read.table("household_power_consumption.txt", sep = ";", col.names = colnames(read.table("household_power_consumption.txt", sep = ";", nrows = 1, header = TRUE)), skip = 1)
+# Subset the data with the only 2 dates concerned by the assignment
 data <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
+# Create a new column with datetime
 data$DataTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
-png("plot2.png", width = 480, height = 480, units = "px")
+
+# Create the graph in png file
+png("plot2.png", width = 480, height = 480, bg = "transparent", units = "px")
 plot(data$DataTime, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,6 +1,10 @@
+# Read the data after unzipped the txt file
 data <- read.table("household_power_consumption.txt", sep = ";", col.names = colnames(read.table("household_power_consumption.txt", sep = ";", nrows = 1, header = TRUE)), skip = 1)
+# Subset the data with the only 2 dates concerned by the assignment
 data <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
+# Create a new column with datetime
 data$DataTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+# format some columns
 data$Sub_metering_1 <- as.character(data$Sub_metering_1)
 data$Sub_metering_1 <- as.numeric(data$Sub_metering_1)
 data$Sub_metering_2 <- as.character(data$Sub_metering_2)
@@ -11,7 +15,7 @@ data$Voltage <- as.character(data$Voltage)
 data$Voltage <- as.numeric(data$Voltage)
 data$Global_reactive_power <- as.character(data$Global_reactive_power)
 data$Global_reactive_power <- as.numeric(data$Global_reactive_power)
-
+# Create the graph in png file
 png("plot4.png", width = 480, height = 480, bg = "transparent", units = "px")
 par(mfrow = c(2, 2))
 with(data, {
