d41d5fdf69f974456ed32e5d7b4827dd5e768c7f
Chtiprog <chtiprog@free.fr>
2015-03-08 03:03:23 -0300

    get and clean the data in one script

--- plot1.R
+++ plot1.R
@@ -1,13 +1,6 @@
-# Read the data after unzipped the txt file
-data <- read.table("household_power_consumption.txt", sep = ";", col.names = colnames(read.table("household_power_consumption.txt", sep = ";", nrows = 1, header = TRUE)), skip = 1)
-# Subset the data with the only 2 dates concerned by the assignment
-data <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
-# Create a new column with datetime
-data$DataTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
-# format some columns
-data$Global_active_power = as.character(data$Global_active_power)
-data$Global_active_power = as.numeric(data$Global_active_power)
+#!/usr/bin/env Rscript
 
+source('getData.R')
 # Create the graph in png file
 png("plot1.png", width = 480, height = 480, bg = "transparent", units = "px")
 plot1 <- hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
--- plot3.R
+++ plot3.R
@@ -1,16 +1,7 @@
-# Read the data after unzipped the txt file
-data <- read.table("household_power_consumption.txt", sep = ";", col.names = colnames(read.table("household_power_consumption.txt", sep = ";", nrows = 1, header = TRUE)), skip = 1)
-# Subset the data with the only 2 dates concerned by the assignment
-data <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
-# Create a new column with datetime
-data$DataTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
-# format some columns
-data$Sub_metering_1 <- as.character(data$Sub_metering_1)
-data$Sub_metering_1 <- as.numeric(data$Sub_metering_1)
-data$Sub_metering_2 <- as.character(data$Sub_metering_2)
-data$Sub_metering_2 <- as.numeric(data$Sub_metering_2)
-data$Sub_metering_3 <- as.character(data$Sub_metering_3)
-data$Sub_metering_3 <- as.numeric(data$Sub_metering_3)
+#!/usr/bin/env Rscript
+
+source('getData.R')
+
 # Create the graph in png file
 png("plot3.png", width = 480, height = 480, bg = "transparent", units = "px")
 plot(data$DataTime, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
